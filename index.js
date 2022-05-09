bbc8ab9ea9fef1c648ea7e0b10b1fc26eba20a74
ManuelPiris <bow6969@gmail.com>
2022-05-09 13:22:38 +0200

    Ejercicio Reto1

---
+++ index.js
@@ -0,0 +1,25 @@
+let resultado;
+function suma(x,y){
+    resultado = x + y;
+    return resultado;
+}
+function resta(x,y){
+    resultado = x - y;
+    return resultado;
+}
+function producto(x,y){
+    resultado = x * y;
+    return resultado;
+}
+function division(x,y){
+    resultado = x / y;
+    return resultado;
+}
+
+function cuadrado(x = 0){
+    resultado = x*x;
+    return resultado;
+}
+
+
+module.exports = {suma,resta,producto,division,cuadrado};
---
+++ run.js
@@ -0,0 +1,7 @@
+let myLib = require("./index");
+console.log(myLib.suma(5,6));
+console.log(myLib.resta(5,6));
+console.log(myLib.producto(5,6));
+console.log(myLib.division(5,6));
+console.log(myLib.cuadrado(8));
+
