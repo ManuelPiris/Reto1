da815d0e1ca4c97bab2fa4a46727f57958f60a28
ManuelPiris <bow6969@gmail.com>
2022-05-09 23:47:07 +0200

    modificado

--- run.js
+++ run.js
@@ -1,5 +1,5 @@
 let myLib = require("./index");
-console.log(myLib.suma(5,6));
+console.log(myLib.suma(5,6,8));
 console.log(myLib.resta(5,6));
 console.log(myLib.producto(5,6));
 console.log(myLib.division(5,6));
